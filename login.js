d289ec01f8a3c8e021873d5b059209dd1846664c
marketmoney22 <jjhello302424@gmail.com>
2025-05-03 01:18:12 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,9 @@
+// login.js
+const clientId = "1367887607702229064";
+const redirectUri = "https://marketmoney22.github.io/callback.html";
+const scope = "identify email";
 
 function loginWithDiscord() {
-  window.location.href = "https://discord.com/oauth2/authorize?client_id=1367887607702229064&response_type=code&redirect_uri=https://marketmoney22.github.io/callback.html&scope=identify+email";
-}
-
-const saved = localStorage.getItem("discord_user");
-if (saved) {
-  const user = JSON.parse(saved);
-  document.getElementById("user-info").innerHTML = `<span>\${user.username}#\${user.discriminator}</span>`;
+  const oauthUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope.replace(" ", "+")}`;
+  window.location.href = oauthUrl;
 }

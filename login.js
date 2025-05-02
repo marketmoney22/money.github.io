dac77f99a2c910ee7602d7ff05d6d13d47960cf4
marketmoney22 <jjhello302424@gmail.com>
2025-05-03 00:45:05 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,10 @@
-// 추후 실제 Discord OAuth 처리용
+
+function loginWithDiscord() {
+  window.location.href = "https://discord.com/oauth2/authorize?client_id=1367887607702229064&response_type=code&redirect_uri=https://marketmoney22.github.io/callback.html&scope=identify+email";
+}
+
+const saved = localStorage.getItem("discord_user");
+if (saved) {
+  const user = JSON.parse(saved);
+  document.getElementById("user-info").innerHTML = `<span>\${user.username}#\${user.discriminator}</span>`;
+}
